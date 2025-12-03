f06a213876f00c1696b3eef148989a94b263e906
sejungkwak <kwak.sejung@gmail.com>
2025-12-03 20:04:08 +0000

    Create installation script for database initialisation

---
+++ install.php
@@ -0,0 +1,13 @@
+<?php
+
+require "lib/config.php";
+
+try {
+    $connection = new PDO("mysql:host=$host", $username, $password, $options);
+
+    $sql = file_get_contents("data/init.sql");
+    $connection->exec($sql);
+    echo "Database and tables created successfully.";
+} catch(PDOException $error) {
+    echo $sql . "<br>" . $error->getMessage();
+}
